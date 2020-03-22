cb2837b72e334059cbb1bcc545c98ce8785474ff
Chase Lindsay <chase.lindsay1@gmail.com>
2020-03-22 13:43:52 -0400

    created cocktails controller and view

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,45 @@
+class CocktailsController < ApplicationController
+  skip_before_action :authenticate_user!
+  before_action :set_cocktail, only: [:show, :edit, :update, :destroy, :calc_occupation]
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @dose = Dose.new
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @cocktail.update(cocktail_params)
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :cocktails, except: [:destroy] do
+  resources :doses, only: [:new, :edit, :update, :create]
+  end
+  resources :doses, only: [:destroy]
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
